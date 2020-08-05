c38dd0a90c200865cde09c93f7f406c26afc7c6d
Zachary Peterson <zp.lambda@yahoo.com>
2020-08-05 18:15:20 -0400

    MVP

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,27 @@
+// import { useLocalStorage} from './useLocalStorage';
+
+// export const useDarkMode = () => {
+//     const [darkMode, setDarkMode] = useLocalStorage("dark-mode");
+
+//     console.log(darkMode);
+
+//     return [darkMode, setDarkMode];
+// }
+
+import { useEffect } from 'react';
+import { useLocalStorage } from './useLocalStorage';
+
+export const useDarkMode = () => {
+    const [someValue, setSomeValue] = useLocalStorage('dark-mode',false);
+    
+    console.log(someValue)
+    
+    useEffect(() => {
+        if (someValue === true){
+            return document.body.classList.add('dark-mode'); 
+        }
+       return document.body.classList.remove('dark-mode')
+    }, [someValue]);
+
+    return [someValue, setSomeValue];
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 
+import {useDarkMode} from './hooks/useDarkMode';
+
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 
@@ -9,6 +11,7 @@ import "./styles.scss";
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
+  // const darkMode = useDarkMode();
 
   useEffect(() => {
     axios
@@ -19,7 +22,7 @@ const App = () => {
       .catch(err => console.log(err));
   }, []);
   return (
-    <div className={darkmode ? "dark-mode App" : "App"}>
+    <div className={'dark-mode' ? "App" : "App"}>
       <Navbar />
       <Charts coinData={coinData} />
     </div>
